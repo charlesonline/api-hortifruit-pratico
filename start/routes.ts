d2aa3b48191e215ace30e0559c298af28c975b04
Charles S Ferreira <charles@easy-tecno.com.br>
2023-06-25 11:53:38 -0300

    Software de consumo de api

--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,11 @@
 import Route from '@ioc:Adonis/Core/Route'
-import User from 'App/Models/User'
+// import User from 'App/Models/User'
 
 Route.get('/', async () => {
-  User.create({
-    email: 'admin@admin.com',
-    password: '123',
-    tipo: 'admin'
-  })
-  // return { hello: 'world' }
+  // User.create({
+  //   email: 'admin2@admin.com',
+  //   password: '123',
+  //   tipo: 'admin'
+  // })
+  return { hello: 'world' }
 })
