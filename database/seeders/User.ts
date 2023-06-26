f0e12c1b79c12773ee273ac384b75c6fd4e85b17
Charles S Ferreira <charles@easy-tecno.com.br>
2023-06-26 11:47:44 -0300

    add seed User

---
+++ database/seeders/User.ts
@@ -0,0 +1,17 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+
+export default class extends BaseSeeder {
+  public async run () {
+    await User.createMany([
+      {
+        email: 'admin@admin.com',
+        password: '123',
+      },
+      {
+        email: 'admin2@admin.com',
+        password: '1234'
+      }
+    ])
+  }
+}
