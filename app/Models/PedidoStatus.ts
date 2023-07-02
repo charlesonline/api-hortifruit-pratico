5674ed1ac1cce2b36cd77ba04421ca1b3aa90297
Charles S Ferreira <charles@easy-tecno.com.br>
2023-07-02 19:44:41 -0300

    Aula 38

---
+++ app/Models/PedidoStatus.ts
@@ -0,0 +1,23 @@
+import { DateTime } from 'luxon'
+import { BaseModel, HasOne, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import Status from './Status'
+
+export default class PedidoStatus extends BaseModel {
+  @column({ isPrimary: true, serializeAs: null })
+  public pedido_id: number
+
+  @column({ isPrimary: true })
+  public status_id: number
+
+  @column()
+  public observacao: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @hasOne(() => Status,{
+    localKey:"status_id",
+    foreignKey:"id"
+  })
+  public status: HasOne<typeof Status>
+}
---
+++ app/Models/Status.ts
@@ -0,0 +1,10 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Status extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+  
+  @column()
+  public status: string
+}
